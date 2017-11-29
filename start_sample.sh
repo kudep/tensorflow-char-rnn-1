4448ba6e726ca60fb2071d4c2f7cad9196f71e4b
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2017-11-29 12:09:44 +0300

    Fixed sample script.

--- start_sample.sh
+++ start_sample.sh
@@ -2,11 +2,12 @@
 
 export model_path="/home/kuznetsov/tmp/thread32"
 # rm -rf ${model_path}/model
-mkdir  ${model_path}/model
-cp -v $0 ${model_path}/model
+# mkdir  ${model_path}/model
+# cp -v $0 ${model_path}/model
 
 
-python train.py \
-  --data_file $model_path/data/sber.txt \
-  --model_path $model_path/best_model/model-72048. \ 
+python sample.py \
+  --model_path $model_path/model/best_model/model-72048 \
+  --start_text="Здравствуйте!" \
+  --length=250 \
   --init_dir ${model_path}/model
